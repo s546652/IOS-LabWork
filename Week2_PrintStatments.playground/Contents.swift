573f914f6f563a1d4b72f0fb59cc766d8c497722
s546652 <98067009+s546652@users.noreply.github.com>
2022-08-23 08:56:31 -0500

    in class week02

--- Week2_PrintStatments.playground/Contents.swift
+++ Week2_PrintStatments.playground/Contents.swift
@@ -3,6 +3,13 @@ import UIKit
 print("Hii",10,12.25)
 var today = "Tuesday"
 today = "08/22/2022"
-print(today)
+//print(today)
 
-fo
+
+//var todays_date:Date  = 08/22/22
+var todays_Date:Date = Date()
+print(todays_Date)
+var name = todays_Date
+
+
+
---
+++ week_2_InClass.playground/Contents.swift
@@ -0,0 +1,87 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Hello world"+greeting)
+print("Hello World\(greeting)")
+
+
+var age = 12
+
+print("My age is \(age)")
+
+//print("My age is "+age)
+//concatenation of different datatypes is not allowed.
+
+print("You are \(age) years old and in another \(age)years, you will be \(age * 2)")
+
+print("""
+Hello
+World!
+   testing with aligment
+""")// triple " for printing in the exact same format
+
+
+
+print("hello all, \rWelcome to Swift programming")
+
+let  welcomeMessage : String = "Hello!"
+   print(welcomeMessage , "All")
+
+let  byeMessage = "Hello!"
+   print(byeMessage , "All")
+   //cannot change value for a let(constant variable)
+   //byeMessage = "test"
+
+var name:String = "Roshni"
+print(name,3,"Sem")
+name = "Kim Nam-Joon"
+print(name,3,"Sem")
+
+
+print("Welcome to Swift Programming")
+print("Fall 2021")
+print("*************")
+print("Welcome to Swift Programming" , terminator : "-" )
+print("Fall 2021")
+
+print("Roshni",terminator: "$")
+print("Gagenapally")
+
+
+print("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: "-")
+
+
+
+
+//Worksheet - 2
+
+var mobileBrand = "Apple"
+mobileBrand = "Samsung"
+print(mobileBrand)
+
+let pi = 3.14
+print(pi)
+
+var age_of_JK : Int = 23
+age_of_JK = age_of_JK * 2
+print(age_of_JK)
+
+var aweMessage = "This is Superb!"
+print(aweMessage)
+print("aweMessage")
+
+
+var course1 = "iOS"
+var course2 = "Java"
+print(course1,course2)
+print(course1,"-",course2)
+
+
+print(10,20,30)
+    print(12.5,15.5)
+
+print(12.5,15.5 ,separator: "^")
