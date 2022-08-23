76689d64ce8e8a97cfd85491a084e3fbab10f95a
s546652 <98067009+s546652@users.noreply.github.com>
2022-08-23 09:20:10 -0500

    Week_02-Class_01

--- week_2_InClass.playground/Contents.swift
+++ week_2_InClass.playground/Contents.swift
@@ -85,3 +85,52 @@ print(10,20,30)
     print(12.5,15.5)
 
 print(12.5,15.5 ,separator: "^")
+
+
+
+
+// Tuples
+
+var httpError = (errorCode :404 , errorMessage : " Page not found")
+print(httpError)
+print(httpError.errorCode , terminator: "-")
+print(httpError.errorMessage)
+
+var name_BTS = ("Kim","Nam-Joon")
+var fName = name_BTS.0
+var lName = name_BTS.1
+print(fName)
+print(fName, lName)
+print("testing -- ",fName,separator: ",",  terminator : " ")
+print(lName)
+print(name_BTS)
+print("printing witout separator")
+print(name_BTS.0,name_BTS.1)
+
+
+var origin = (x : 0 , y : 0)
+var point = origin
+print(point)
+
+
+let city = (name : "Maryville" , population : 11,000)
+let ( cityName ,cityPopulation ) = (city.0 , city.1)
+print(cityName)
+print(cityPopulation)
+
+let groceries = ("bread","onions")
+print(groceries.0)
+print(groceries.1)
+print(type(of: groceries))
+
+var fname = "Joe"
+var lname = "Root"
+(fname , lname) = (lname , fname)
+print("First Name is \(fname) and Last Name is \(lname)")
+
+
+var cricketKit = ("handGloves" ,"helmet",("bat","ball"))
+print(cricketKit.0)
+print(cricketKit.1)
+print(cricketKit.2.0)
+print(cricketKit.2.1)
