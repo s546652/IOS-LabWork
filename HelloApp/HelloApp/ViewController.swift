e8fdaa15511a9f963c59565bf4348b5eb49d414e
s546652 <98067009+s546652@users.noreply.github.com>
2022-08-25 09:50:01 -0500

    1st app

---
+++ HelloApp/HelloApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Gagenapally,Roshni Damodar Reddy on 8/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameOutlet: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
