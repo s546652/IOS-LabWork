c2a8acefbf5aa58fdc0d533475368c41a84d2842
s546652 <98067009+s546652@users.noreply.github.com>
2022-11-29 08:05:34 -0600

    commit

---
+++ Gagenapally_MovieDisplayApp/Gagenapally_MovieDisplayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gagenapally_MovieDisplayApp
+//
+//  Created by Gagenapally,Roshni Damodar Reddy on 11/29/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
